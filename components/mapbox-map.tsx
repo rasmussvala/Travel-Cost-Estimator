a1ae04a973f115399f0f0073c51ff8b769e973f9
Rasmus Svala <rassv453@student.liu.se>
2025-08-24 18:01:17 +0200

    chore: added dark and light mode support for map

--- components/mapbox-map.tsx
+++ components/mapbox-map.tsx
@@ -1,33 +1,66 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { useTheme } from "next-themes";
 
 export default function MapboxMap() {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const mapRef = useRef<mapboxgl.Map | null>(null);
+  const { resolvedTheme } = useTheme();
+  const [ismounted, setIsMounted] = useState(false);
 
   const token = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "";
 
+  // Create map on mount
   useEffect(() => {
+    // Early return if no container or map already initialized
     if (!mapContainer.current) return;
+    if (mapRef.current) return;
 
     mapboxgl.accessToken = token;
 
     mapRef.current = new mapboxgl.Map({
       container: mapContainer.current,
+      style: "mapbox://styles/mapbox/standard",
+      config: {
+        basemap: {
+          lightPreset: "day",
+        },
+      },
       center: [18, 59],
       zoom: 5,
     });
 
     mapRef.current.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    return () => {
+      mapRef.current?.remove();
+    };
   }, [token]);
+
+  // Sorry for this...
+  // Set isMounted to true after a short delay to ensure theme is loaded
+  useEffect(() => {
+    setTimeout(() => {
+      setIsMounted(true);
+    }, 100);
+  }, []);
+
+  // Update map when theme changes and one time in the beginning
+  useEffect(() => {
+    if (!mapRef.current) return;
+
+    mapRef.current.setConfig("basemap", {
+      lightPreset: resolvedTheme === "dark" ? "night" : "day",
+    });
+  }, [resolvedTheme, ismounted]);
 
   return (
     <div
       ref={mapContainer}
-      className={"w-full h-[400px] rounded-lg overflow-hidden"}
+      className="w-full h-[400px] rounded-lg overflow-hidden"
     />
   );
 }
